7571ab3c950145f22e42e4bbbc616baaf9747ccc
Eh3unm0radi <eh3unm0radi@gmail.com>
2024-09-15 22:15:15 +0330

    second commit

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('generate-token',[GameController::class,'generateToken']);
+Route::post('/generate-token',[GameController::class,'generateToken']);
