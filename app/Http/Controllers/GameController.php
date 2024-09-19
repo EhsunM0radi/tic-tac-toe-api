56844e6796c28af263580fd12a69f5fe7fce19f3
Eh3unm0radi <eh3unm0radi@gmail.com>
2024-09-19 09:03:49 +0330

    third commit

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -11,11 +11,17 @@ class GameController extends Controller
     public function generateToken(Request $request)
     {
         $token = Str::uuid()->toString();
-        
+
         GameTokens::create([
             "token" => $token,
         ]);
 
         return response()->json(['message' => 'Game saved successfully'], 200);
+    }
+
+    public function gameTokens(Request $request)
+    {
+        $tokens = GameTokens::select('id','token')->get();
+        return response()->json($tokens,200);
     }
 }
