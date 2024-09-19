144bb243cce77e15d9463ce16a116d8739af598d
ehsan <eh3unm0radi@gmail.com>
2024-09-19 17:45:52 +0330

    4th commit

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -8,12 +8,28 @@ use Illuminate\Support\Str;
 
 class GameController extends Controller
 {
+    private function generateBoardString(array $data)
+    {
+        // استخراج آخرین وضعیت صفحه
+        $finalSquares = $data['moves'][count($data['moves']) - 1]['squares'];
+
+        // ایجاد رشته نهایی
+        $boardString = implode('', array_map(function($cell) {
+            return $cell ?? 'O'; // خانه‌های خالی را به 'O' تبدیل می‌کنیم
+        }, $finalSquares));
+
+        return $boardString;
+    }
+
     public function generateToken(Request $request)
     {
+        $gameData = $request->all();
+        $boardString = $this->generateBoardString($gameData);
         $token = Str::uuid()->toString();
 
         GameTokens::create([
             "token" => $token,
+            "final_map" => $boardString
         ]);
 
         return response()->json(['message' => 'Game saved successfully'], 200);
